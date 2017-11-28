215cfbeceb557a7c9f84f842dc15f8265879da15
leofly <271941476@qq.com>
2017-11-28 11:10:16 +0800

    修改大小写

--- get_douban250.py
+++ get_douban250.py
@@ -7,12 +7,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_html_text(url, k):
+def get_html_text(url, start_arg):
     try:
-        if(k == 0):
+        if start_arg == 0:
             kw = {}
         else:
-            kw = {'start': k, 'filter': ''}
+            kw = {'start': start_arg, 'filter': ''}
         r = requests.get(url, params=kw, headers={'User-Agent': 'Mozilla/4.0'})
         r.raise_for_status()
         r.encoding = r.apparent_encoding
@@ -24,32 +24,31 @@ def get_html_text(url, k):
 def get_data(html):
     soup = BeautifulSoup(html, "lxml")
     # 找到第一个class属性值为grid_view的ol标签
-    movieList = soup.find('ol', attrs={'class': 'grid_view'})
-    moveInfo = []
-    for movieLi in movieList.find_all('li'):  # 找到所有li标签
+    movie_list = soup.find('ol', attrs={'class': 'grid_view'})
+    for movieLi in movie_list.find_all('li'):  # 找到所有li标签
         data = []
         # 得到电影名字
         # 找到第一个class属性值为hd的div标签
-        movieHd = movieLi.find('div', attrs={'class': 'hd'})
+        movie_hd = movieLi.find('div', attrs={'class': 'hd'})
         # 找到第一个class属性值为title的span标签
-        movieName = movieHd.find('span', attrs={'class': 'title'}).getText()
+        movie_name = movie_hd.find('span', attrs={'class': 'title'}).getText()
         # 也可使用.string方法
-        data.append(movieName)
+        data.append(movie_name)
 
         # 得到电影的评分
-        movieScore = movieLi.find(
+        movie_score = movieLi.find(
             'span', attrs={'class': 'rating_num'}).getText()
-        data.append(movieScore)
+        data.append(movie_score)
 
         # 得到电影的评价人数
-        movieEval = movieLi.find('div', attrs={'class': 'star'})
-        movieEvalNum = re.findall(r'\d+', str(movieEval))[-1]
-        data.append(movieEvalNum)
+        movie_eval = movieLi.find('div', attrs={'class': 'star'})
+        movie_eval_num = re.findall(r'\d+', str(movie_eval))[-1]
+        data.append(movie_eval_num)
 
         # 得到电影的短评
-        movieQuote = movieLi.find('span', attrs={'class': 'inq'})
-        if(movieQuote):
-            data.append(movieQuote.getText())
+        movie_quote = movieLi.find('span', attrs={'class': 'inq'})
+        if movie_quote:
+            data.append(movie_quote.getText())
         else:
             data.append("无")
 
@@ -59,9 +58,8 @@ def get_data(html):
 
 # 将输出重定向到txt文件
 output = sys.stdout
-outputfile = open("moviedata.txt", 'w', encoding='utf-8')
-sys.stdout = outputfile
-
+outputFile = open("moviedata.txt", 'w', encoding='utf-8')
+sys.stdout = outputFile
 
 outputMode = "{0:{4}^20}\t{1:^10}\t{2:^10}\t{3:{4}<10}"
 print(outputMode.format('电影名称', '评分', '评论人数', '短评', chr(12288)))
@@ -73,6 +71,5 @@ while k <= 225:
     k += 25
     get_data(html)
 
-
-outputfile.close()
+outputFile.close()
 sys.stdout = output
