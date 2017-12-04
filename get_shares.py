7a4b4d34706d98fab66a949dbef2b8736e7e5194
xiang-su <271941476@qq.com>
2017-12-04 21:44:48 +0800

    little change

--- get_shares.py
+++ get_shares.py
@@ -1,10 +1,11 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import tushare as ts
+import numpy as np
 from matplotlib.pylab import datestr2num
 
 # 设置中文字体
-font = matplotlib.font_manager.FontProperties(fname='C:\Windows\Fonts\simsun.ttc')
+font = matplotlib.font_manager.FontProperties(fname='/System/Library/Fonts/PingFang.ttc')
 
 # 获取实时行情数据
 hq = ts.get_today_all()
@@ -15,8 +16,7 @@ hq = hq[['code', 'name', 'changepercent', 'trade']]
 mins = hq.trade > 0.00
 maxs = hq.trade <= 2.99
 allselect = mins & maxs
-# data = hq[allselect].sort('trade')
-data = hq[allselect]
+data = hq[allselect]# .sort('trade')
 
 # 提取低价股股票代码,并剔除深圳成指股票
 code_list = []
@@ -24,40 +24,45 @@ for c in data.code:
     if c[0] != "0":
         code_list.append(c)
 code_list
+print(code_list)
 
-sh_hist_data = ts.get_hist_data(code='sh', start='2014-12-01', end='2016-12-01')
+sh_hist_data = ts.get_hist_data(code='sh', start='2016-12-01', end='2017-12-01')
 sh_hist_data = sh_hist_data[['open', 'high', 'close', 'low', 'volume', 'price_change', 'p_change']].sort_index()
 
 sh_hist_data.head(10)
 dates = [datestr2num(i) for i in sh_hist_data.index]
-plt.figure(figsize=(40, 30))
+plt.figure(figsize=(40,30))
 # 新建一个子图，绘制上证指数走势
 plt.subplot(311)
-plt.title("2014年12月1日至2016年12月1日上证指数最低价走势", fontsize=30)
-plt.xticks(rotation=20, fontsize=26)
+plt.title("2014年12月1日至2016年12月1日上证指数最低价走势",fontsize=30)
+plt.xticks(rotation=20,fontsize=26)
 plt.yticks(fontsize=26)
-plt.plot_date(dates, sh_hist_data.low, '-', linewidth=2.5)
+plt.plot_date(dates,sh_hist_data.low,'-',linewidth=2.5)
 plt.grid(True)
 # 遍历低价股代码列表，绘制股价走势
 for i in code_list[0:4]:
-    hist_data = ts.get_hist_data(code=i, start='2014-12-01', end='2016-12-01')
-    code_data = hist_data[['open', 'high', 'close', 'low', 'volume', 'price_change', 'p_change']].sort_index()
+    hist_data = ts.get_hist_data(code=i,start='2016-12-01',end='2017-12-01')
+    code_data = hist_data[['open','high','close','low','volume','price_change','p_change']].sort_index()
     dates = [datestr2num(t) for t in code_data.index]
     plt.subplot(312)
-    plt.title("2014年12月1日至2016年12月1日各低价股最低价走势", fontsize=30)
-    plt.xticks(rotation=20, fontsize=26)
+    plt.title("2014年12月1日至2016年12月1日各低价股最低价走势",fontsize=30)
+    plt.xticks(rotation=20,fontsize=26)
     plt.yticks(fontsize=26)
-    plt.plot_date(dates, code_data['low'], '-', linewidth=2.5, label=i)
-    plt.legend(loc=1, fontsize=22)
+    plt.plot_date(dates,code_data['low'],'-',linewidth=2.5,label=i)
+    plt.legend(loc=1,fontsize=22)
     plt.grid(True)
 for i in code_list[4:]:
-    hist_data = ts.get_hist_data(code=i, start='2014-12-01', end='2016-12-01')
-    code_data = hist_data[['open', 'high', 'close', 'low', 'volume', 'price_change', 'p_change']].sort_index()
+    hist_data = ts.get_hist_data(code=i,start='2016-12-01',end='2017-12-01')
+    code_data = hist_data[['open','high','close','low','volume','price_change','p_change']].sort_index()
     dates = [datestr2num(t) for t in code_data.index]
     plt.subplot(313)
-    plt.title("2014年12月1日至2016年12月1日各低价股最低价价走势", fontsize=30)
-    plt.xticks(rotation=20, fontsize=26)
+    plt.title("2014年12月1日至2016年12月1日各低价股最低价价走势",fontsize=30)
+    plt.xticks(rotation=20,fontsize=26)
     plt.yticks(fontsize=26)
-    plt.plot_date(dates, code_data['low'], '-', linewidth=2.5, label=i)
-    plt.legend(loc=1, fontsize=22)
+    plt.plot_date(dates,code_data['low'],'-',linewidth=2.5,label=i)
+    plt.legend(loc=1,fontsize=22)
     plt.grid(True)
+
+plt.show()
+
+print("success")
--- get_weather.py
+++ get_weather.py
@@ -1,15 +1,16 @@
 """
-This is a python-demo to get contry's weather in five days
+This is a python-demo to get country's weather in five days
 """
-from urllib.request import urlopen
-
+import requests
 from bs4 import BeautifulSoup
 
 URL = 'http://www.weather.com.cn/weather/101190101.shtml'
-BS = BeautifulSoup(urlopen(URL), 'lxml')
-WEATHERLIST = BS.findAll("li", {'class': {'sky skyid lv3 on',
+r = requests.get(URL)
+r.encoding = 'utf-8'
+bs = BeautifulSoup(r.text, 'lxml')
+weather_list = bs.findAll("li", {'class': {'sky skyid lv3 on',
                                           'sky skyid lv3', 'sky skyid lv2', 'sky skyid lv1'}})
 print('NanJing weather:\n')
-for i in WEATHERLIST:
+for i in weather_list:
     print(i.get_text().replace('\n', ' '))
-WEATHERLIST = BS.findAll()
+weather_list = bs.findAll()
